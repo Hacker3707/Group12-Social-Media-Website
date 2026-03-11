2e3436114a80347bc4644610ac3f861661ca6bbe
khangho31241023603-star <khangho.31241023603@st.ueh.edu.vn>
2026-03-11 22:28:08 +0700

    Create Video.php

---
+++ MVC/Model/MVC/Model/Video.php
@@ -0,0 +1,23 @@
+<?php
+require_once "Media.php";
+
+class Video extends Media
+{
+    public $duration;
+    public $format;
+
+    public function createVideo()
+    {
+        $query = "INSERT INTO media(post_id, media_url, created_at)
+                  VALUES(:post_id, :media_url, NOW())";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":post_id", $this->post_id);
+        $stmt->bindParam(":media_url", $this->media_url);
+
+        return $stmt->execute();
+    }
+}
+
+?>
