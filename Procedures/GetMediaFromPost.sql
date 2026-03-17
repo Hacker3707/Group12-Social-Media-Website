da6b2bf532a5a050e438839691f9b311070c716e
khangho31241023603-star <khangho.31241023603@st.ueh.edu.vn>
2026-03-18 00:40:18 +0700

    Update and rename GetMediaFromPost.spl to GetMediaFromPost.sql

--- Procedures/GetMediaFromPost.sql
+++ Procedures/GetMediaFromPost.sql
@@ -3,9 +3,10 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS getMediaFromPost$$
 CREATE PROCEDURE getMediaFromPost(IN postId INT)
 BEGIN
-    SELECT *
-    FROM media
-    WHERE post_id = postId;
+    SELECT m.*
+    FROM media m
+    JOIN post_media pm ON m.media_id = pm.media_id
+    WHERE pm.post_id = postId;
 END$$
 
 DELIMITER ;
