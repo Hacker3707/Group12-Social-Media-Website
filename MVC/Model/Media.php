61486453412b859c222848b584b6b625f6ec5687
khangho31241023603-star <khangho.31241023603@st.ueh.edu.vn>
2026-03-21 12:15:51 +0700

    fix: update Media model to match SQL schema

--- MVC/Model/Media.php
+++ MVC/Model/Media.php
@@ -5,25 +5,72 @@ class Media
     protected $conn;
     protected $table = "media";
 
-    public $id;
-    public $post_id;
-    public $media_url;
-    public $created_at;
+    // Khớp đúng bảng media trong SQL:
+    // MediaID | PostID | UserID | MediaType
+    public $MediaID;
+    public $PostID;
+    public $UserID;
+    public $MediaType;
 
     public function __construct($db)
     {
         $this->conn = $db;
     }
 
-    public function getMediaByPost($post_id)
+    // Lấy tất cả media của một bài post
+    public function getMediaByPost($PostID)
     {
-        $query = "SELECT * FROM media WHERE post_id = :post_id";
+        $query = "SELECT * FROM " . $this->table . "
+                  WHERE PostID = :PostID";
 
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(":post_id", $post_id);
+        $stmt->bindParam(":PostID", $PostID);
         $stmt->execute();
 
         return $stmt;
+    }
+
+    // Lấy tất cả media do một user upload
+    public function getMediaByUser($UserID)
+    {
+        $query = "SELECT * FROM " . $this->table . "
+                  WHERE UserID = :UserID";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":UserID", $UserID);
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    // Thêm media mới
+    public function create()
+    {
+        $query = "INSERT INTO " . $this->table . " (PostID, UserID, MediaType)
+                  VALUES (:PostID, :UserID, :MediaType)";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":PostID",    $this->PostID);
+        $stmt->bindParam(":UserID",    $this->UserID);
+        $stmt->bindParam(":MediaType", $this->MediaType);
+
+        if ($stmt->execute()) {
+            $this->MediaID = $this->conn->lastInsertId();
+            return true;
+        }
+        return false;
+    }
+
+    // Xoá media theo MediaID
+    public function delete()
+    {
+        $query = "DELETE FROM " . $this->table . "
+                  WHERE MediaID = :MediaID";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":MediaID", $this->MediaID);
+
+        return $stmt->execute();
     }
 }
 
